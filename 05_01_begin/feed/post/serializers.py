61e5fb90b1a36768d9c6de04b76cf300ede0ba72
Ronnie Sheer <ronysheer@gmail.com>
2024-10-29 12:52:58 +0000

    05_01_begin

---
+++ 05_01_begin/feed/post/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        lookup_field = 'slug'
+        fields = '__all__'
+
+    created = serializers.DateTimeField(format='%a, %d %b %Y')
+    
